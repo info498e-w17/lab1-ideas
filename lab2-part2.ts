1629a283870b5d1945fb1040d5b656e717b67fa9
hiiamhuy <hiiamhuy@uw.edu>
2017-01-10 11:09:57 -0800

    Part 2 finished
    completed part 2

---
+++ lab2-part2.ts
@@ -0,0 +1,40 @@
+
+/**
+ * name
+ */
+class bankSaving {
+    private balance:number;
+    private name:string;
+    private withdrawLimit:number;
+    constructor(initialBalance:number, ownerName:string) {
+        this.balance = initialBalance;
+        this.name = ownerName;
+        this.withdrawLimit = 3;
+    }
+
+    deposit(amount:number) {
+        this.balance = this.balance + amount;
+    }
+
+    withdraw(amount:number) {
+        if(this.withdrawLimit > 0) {
+            this.withdrawLimit = this.withdrawLimit - 1;
+            this.balance = this.balance - amount;
+        } else {
+            console.log(`You are only allowed 3 withdraw limits this is the current amout of time you have left ${this.withdrawLimit}`);
+        }
+    }
+
+    checkBalance():void {
+        console.log(`This balance is ${this.balance} and the owner is ${this.name}`);
+    }
+}
+
+var bankBECU = new bankSaving(1000, 'Huy Nguyen');
+
+bankBECU.deposit(100);
+bankBECU.withdraw(99);
+bankBECU.withdraw(99);
+bankBECU.withdraw(99);
+bankBECU.withdraw(99);
+bankBECU.checkBalance();
